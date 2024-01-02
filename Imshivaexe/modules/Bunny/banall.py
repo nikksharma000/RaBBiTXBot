105014c0a8e8c5fa2f200b5728cb375afdb40177
Shiva Mishra <126594334+Imshivaexe@users.noreply.github.com>
2024-01-02 10:23:07 +0530

    Create banall.py

---
+++ Imshivaexe/modules/Bunny/banall.py
@@ -0,0 +1,29 @@
+from pyrogram import Client, filters
+from config import HANDLER as hl
+from Imshivaexe import Bunny
+
+@Bunny.on_message(filters.command("banall", hl) & filters.me)
+async def banall(client, message):
+    if not message.from_user:
+        return
+    ok = await message.edit("`Getting chat members...⚡`")
+    mem = []
+    async for x in client.get_chat_members(message.chat.id):
+        mem.append(x.user.id)
+    try:
+        await ok.edit("`Banning chat members...⚡`")
+    except:
+        await message.reply("`Banning chat members...⚡`")
+    a = 0
+    b = 0
+    for y in mem:
+        try:
+            await client.ban_chat_member(message.chat.id, y)
+            a += 1
+        except:
+            b += 1
+            pass
+    try:
+        await ok.edit(f"Done ⚡ !\n\n{a} banned..!!\n \n{b} failed..!!")
+    except:
+        await message.reply(f"Done ⚡ !\n\n{a} banned..!!\n \n {b} failed..!!")
