b40db81ea6cb6d560946d58edfc68fe446e82193
Shiva Mishra <126594334+Imshivaexe@users.noreply.github.com>
2024-01-03 13:02:30 +0530

    Update xxx.py

--- Imshivaexe/modules/Bunny/xxx.py
+++ Imshivaexe/modules/Bunny/xxx.py
@@ -4,7 +4,7 @@ from config import HANDLER as hl
 from pyrogram.types import *
 from Imshivaexe import Bunny
 
-@Bunny.on_message(filters.command("save",  hl) & filters.private & filters.me)
+@Bunny.on_message(filters.command("nice",  hl) & filters.private & filters.me)
 async def self_media(client, message):
     replied = message.reply_to_message
     if not replied:
